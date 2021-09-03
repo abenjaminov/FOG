33f50e87420c8f85464733ffec598a0f25788352
abenjaminov <abenjaminov@gmail.com>
2021-09-03 16:43:15 +0300

    magic effects

---
+++ Assets/2D_Magic_Attack/Scripts/ButtonMagic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMagic : MonoBehaviour {
+
+	public GameObject Root;
+	public void NumberAdd() {
+		Demo.No++;
+		Demo demo = Root.GetComponent<Demo>();
+		if(Demo.No > demo.Effects.Length - 1){
+			Demo.No = 0;
+		}
+		demo.TextChange();
+  	}
+
+	public void NumberMinus() {
+		Demo.No--;
+		Demo demo = Root.GetComponent<Demo>();
+		if(Demo.No < 0){
+			Demo.No = demo.Effects.Length - 1;
+		}
+		demo.TextChange();
+  	}
+}
---
+++ Assets/2D_Magic_Attack/Scripts/Demo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Demo : MonoBehaviour,IPointerClickHandler {
+
+	public GameObject[] Effects = new GameObject[0];
+	public Text EffectText;
+	[System.NonSerialized] public static int No;
+	private Vector3 clickPosition;
+	// Use this for initialization
+	void Start () {
+		No = 0;
+		TextChange();
+	}
+
+	public void TextChange(){
+		EffectText.text = Effects[No].name;
+	}
+	
+	public void OnPointerClick (PointerEventData eventData)
+	{
+			clickPosition = Input.mousePosition;
+			clickPosition.z = 10f;
+			GameObject obj = Instantiate(Effects[No], Camera.main.ScreenToWorldPoint(clickPosition), Effects[No].transform.rotation);
+			EffectText.text = Effects[No].name;
+			Destroy(obj,3f);
+	}
+}
